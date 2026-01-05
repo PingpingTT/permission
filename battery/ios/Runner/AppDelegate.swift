135db40b28ee02aae76e0a682e121cc3becc9dd4
chidchai <chidchai@digio.co.th>
2026-01-05 10:28:30 +0700

    permission

--- battery/ios/Runner/AppDelegate.swift
+++ battery/ios/Runner/AppDelegate.swift
@@ -1,13 +1,40 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+
+    let controller = window?.rootViewController as! FlutterViewController
+
+    let batteryChannel = FlutterMethodChannel(
+      name: "samples.flutter.dev/battery",
+      binaryMessenger: controller.binaryMessenger
+    )
+
+    batteryChannel.setMethodCallHandler { (call, result) in
+      if call.method == "getBatteryLevel" {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        let batteryLevel = UIDevice.current.batteryLevel
+
+        if batteryLevel < 0 {
+          result(FlutterError(
+            code: "UNAVAILABLE",
+            message: "Battery level not available",
+            details: nil
+          ))
+        } else {
+          result(Int(batteryLevel * 100))
+        }
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
